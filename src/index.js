ee904ea3b2672b2d4b1d13d5158c8043e717b7b5
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-03-11 14:30:54 +0300

    9 of 10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (prop) => prop;
+
+const createNotEnumerableProperty = (prop) => {
+  Object.defineProperty(Object.prototype, prop, { value: 'value', enumerable: false });
+  return prop;  
+};
+const createProtoMagicObject = () => {
+  let magic = () => {};
+  magic.prototype = magic.__proto__;
+  return magic;
+};
+
+Function.prototype.valueOf = () => i;
+let i = 0;
+const incrementor = () => {
+  i++;
+  return incrementor;
+};
+
+let j = 0;
+const asyncIncrementor = () => new Promise((resolve) => {
+  j++;
+  return resolve(j);
+});
+
+
+const createIncrementer = () => {
+  function* incr() {
+    for (let i = 1; i <10; i++)
+    {yield i;}
+  };
+  return incr();
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
+const returnBackInSecond = (param) => new Promise((resolve) => {
+    setTimeout(() => resolve(param), 1100);
+  });
+
+const getDeepPropertiesCount = (obj) => {
+  let k = 0;
+  let counter = (obj) => {
+    for (let keys in obj) {
+      if (typeof obj[keys] === "object") {
+        counter(obj[keys]);
+      }
+      k++;
+    }
+    return k;
+  }
+  return counter(obj);
+};
+
+const createSerializedObject = () => {
+  let obj = null;
+  return obj;
+};
+
 const sortByProto = () => {};
 
 exports.createEnumerableProperty = createEnumerableProperty;
